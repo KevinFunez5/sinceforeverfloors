fe6630c3cb030940e84de0335e5732884ddf2468
Kevin Funez <kevin.funez315@gmail.com>
2021-12-09 14:55:53 -0800

    add home and section and styled components

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+import section from './Section'
+
+function Home() {
+  return (
+    <div>
+      <Section />
+    </div>
+  )
+}
+
+export default Home
+
+const Container = styled.div `
+    height: 100vh;
+`
